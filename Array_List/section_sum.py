3114247e30b7145f8972b40c3c48b4eb8f9aee69
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-04-18 23:01:25 +0900

    구간합**

---
+++ Array_List/section_sum.py
@@ -0,0 +1,22 @@
+# 백준 11660
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+A = [[0] * (n +  1)]
+D = [ [0] * (n + 1) for _ in range(n+1)]
+
+for i in range(n):
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
+
+# 합 배열(구간합)
+for i in range(1, n +1):
+    for j in range(1, n+1):
+        D[i][j] = D[i][j-1] + D[i-1][j] - D[i -1][j - 1] + A[i][j]
+
+# 질의 값 출력
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1]
+    print(result)
