a1e87f0b6daedd331c049c9d401324a3c6d7868b
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-05-03 21:23:40 +0900

    stack- 백준

--- DataStructure/stack1.py
+++ DataStructure/stack1.py
@@ -5,5 +5,16 @@ A = list(map(int, input().split))
 myStack = []
 
 for i in range(n):
-    while myStack and A[myStack[-1]]:
-        
+    while myStack and A[myStack[-1]] < A[i]: #오큰수 조건
+        ans[myStack.pop()] = A[i] #정답 리스트에 오큰수 저장
+    myStack.append(i)
+
+while myStack:
+    ans[myStack.pop()] = -1
+
+result = ""
+for i in range(n):
+    result += str(ans[i]) + " "
+
+print(result)
+
