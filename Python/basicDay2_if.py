0e007f4bb71ab0f81290bbf7fa3d828d749d4728
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-09-14 19:50:47 +0900

    100일도전2틀차

---
+++ Python/basicDay2_if.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if A >= 90 or A <= 100:
+    print("A")
+elif A >= 80 or A <= 89:
+    print("B")
+elif A  >= 70 or A <= 79:
+    print("C")
+elif A >= 60 or A <= 69:
+    print("D")
+else:
+    print("F")
---
+++ Python/basicDay3_if.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+if a > b:
+    # a가 b보다 큰 경우
+    print('>')
+elif a < b:
+    print('<')
+
+elif a == b:
+    print('==')
+    
+    
