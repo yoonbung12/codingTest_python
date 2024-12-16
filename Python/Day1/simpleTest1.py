2a1ae6f25bff0fde443e371e3edda8f935425606
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-12-16 21:18:50 +0900

    Easy test

---
+++ Python/Day1/simpleTest1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+whereLive =  input("What's the name of the city your grew up in? \n")
+petName = input("What's your pet's name? \n" )
+
+
+print("Your band name could be " + " " + whereLive + " " + petName)
