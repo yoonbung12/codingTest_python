5f8638516f5db7e08410d4644b337495d4081272
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-12-16 21:03:23 +0900

    variable & print

---
+++ Python/Day1/task.py
@@ -0,0 +1,23 @@
+print("Hello world")
+
+print("Hello World \nHello World!!")
+
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print("New lines can be created with a \n and the letter n")
+#
+# print("My name is" + " "  +input("What is your name?"))
+
+userName = (input("what is your nmme?"))
+length =  len(userName)
+print("이름의 길이는?"  , length)
+
+# 변수 실습
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
