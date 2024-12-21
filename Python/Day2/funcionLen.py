6aaff0cb8c29b729d6d0f469608156bbac3f54e5
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-12-21 16:18:03 +0900

    Data type Convert

---
+++ Python/Day2/funcionLen.py
@@ -0,0 +1,16 @@
+# len 글자의 갯수..
+print(len("123456"))
+
+# type() ----- 타입 확인
+print(type("aaa"))
+print(type(1234))
+print(type(1234.111))
+
+
+intA = "123"
+intB = "456"
+
+print(int(intA) + int(intB))
+
+print("Number of letters in your name:" + str(len(input("Enter Your Name?"))))
+
