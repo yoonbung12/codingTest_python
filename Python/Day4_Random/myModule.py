8f37fd423f57f1b9e9da44fc1cf10eb68a1537d4
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-12-29 17:19:43 +0900

    Random테스트

---
+++ Python/Day4_Random/myModule.py
@@ -0,0 +1,1 @@
+myFavoriteNumber = 3.14151
---
+++ Python/Day4_Random/task.py
@@ -0,0 +1,24 @@
+import random
+import myModule
+
+# randomNumber = random.randint(1, 20)
+#
+# print(randomNumber)
+#
+# print(myModule.myFavoriteNumber)
+
+# randomNumber_0_to_1 = random.random() * 10
+# print(randomNumber_0_to_1)
+
+# randomFloat = random.uniform(1, 20)
+# print(randomFloat)
+
+randomText = random.randint(0,1)
+print(randomText)
+a = "Heads"
+b = "Foots"
+
+if randomText == 0:
+    print(a)
+else:
+    print(b)
