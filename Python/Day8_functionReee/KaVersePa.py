96668064a125d60ef85beb25b474686cecf1fcb1
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2025-03-22 16:36:53 +0900

    위치 인자, 키워드 인자

---
+++ Python/Day8_functionReee/KaVersePa.py
@@ -0,0 +1,12 @@
+
+# 위치 인자 VS 키워드 인자
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("bonggu", "Seoul")
+
+# Keyword Arguments
+greet_with(name="bongguDak", location="Tokyo")
+
+
---
+++ Python/Day8_functionReee/Test.py
@@ -0,0 +1,20 @@
+# 임시 테스중
+# def calculate_love_score(name1, name2):
+#     if "T" == name1.find("T"):
+#         count = count(name1.find("T"))
+#         print(f"F는 {count}")
+#         print(name1)
+#     elif "R" == string.find(name1):
+#         count2 = count(name1.find("R"))
+#         print(f"R은 {count2}")
+#     elif "U" == string.find(name1):
+#         count3 = count(name1.find("R"))
+#         print(f"R은 {count3}")
+#     elif "E" == string.find(name1):
+#         count4 = count(name1.find("E"))
+#         print(f"E는 {count4}")
+#
+#     print(f"Total = {count + count2 + count3 + count4}")
+#
+#
+# calculate_love_score("Byeongwoo Yun", "TuiBasa")
